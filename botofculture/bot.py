a087daa90ebc0339d09d7b353ad521f66098d706
Daya Adianto <addianto@users.noreply.github.com>
2020-05-21 06:28:44 +0700

    Update logging at bot.py

--- botofculture/bot.py
+++ botofculture/bot.py
@@ -1,4 +1,3 @@
-from . import pixiv
 import discord
 import logging
 import sys
@@ -7,9 +6,9 @@ client = discord.Client()
 
 @client.event
 async def on_error(event: str, *args, **kwargs):
-    logging.exception(msg=event,
-                      exc_info=sys.exc_info(),
-                      stack_info=True)
+    logging.error(f'Encountered error: {event}',
+                  exc_info=sys.exc_info(),
+                  stack_info=True)
 
 
 @client.event
@@ -17,23 +16,14 @@ async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    logging.debug(f'{message.author.name}: {message.content}')
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    await pixiv.handle(message)
+    logging.debug(f'{message.author.name} AKA {message.author.display_name} at {message.guild.name}')  # noqa
+    logging.debug(f'> {message.content}')
 
 
 @client.event
 async def on_ready():
     logging.info(f'Logged in as {client.user.name} with ID {client.user.id}')
-
-
-@client.event
-async def on_disconnect():
-    logging.info('Disconnecting from Discord')
-    await pixiv.session.close()
+    logging.info(f'Member of the following guild(s): {client.guilds}')
 
 
 def run(token: str):
